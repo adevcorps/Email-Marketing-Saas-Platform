62e1d0962603ecb9245e32513964405692988af9
AlbertSibiga <albert.devguy@gmail.com>
2023-09-12 10:51:04 -0700

    Project Upload

---
+++ src/layouts/header/HeaderBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto">
+    <div>This is logo</div>
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/products">Products</router-link>
+      <router-link to="/resources">Resources</router-link>
+      <router-link to="/pricing">Pricing</router-link>
+      <router-link to="/contact">Contact Us</router-link>
+    </div>
+    <div>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  </div>
+</template>
