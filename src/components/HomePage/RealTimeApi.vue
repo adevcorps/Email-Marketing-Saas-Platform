86d0646bb4b71a72c890e6166c3519ec8b501592
AlbertSibiga <albert.devguy@gmail.com>
2023-09-15 17:05:54 -0700

    Albert update

--- src/components/HomePage/RealTimeApi.vue
+++ src/components/HomePage/RealTimeApi.vue
@@ -1,33 +1,39 @@
 <template>
-  <section class="relative">
-    <img
+  <section class="relative bg-[url('@/assets/images/home/real-time/bg.png')] bg-no-repeat bg-cover bg-center">
+    <!-- <img
       src="@/assets/images/home/real-time/bg.png"
-      class="absolute w-full h-full object-fill z-0 right-0 top-0 left-0"
+      class="absolute w-full object-fill z-0 right-0 top-0 left-0"
+    /> -->
+    <img
+      src="@/assets/images/home/bulk-email/Polygon-1.png"
+      class="absolute left-0 bottom-3"
     />
+    <img src="@/assets/images/home/bulk-email/poly2.png" class="absolute right-0 top-5" />
     <div class="mx-auto container">
-      <div class="bg-contain justify-between relative flex flex-wrap mt-24 p-20">
-        <img
-          src="@/assets/images/home/bulk-email/poly2.png"
-          class="absolute right-0 top-0"
-        />
+      <div class="bg-contain justify-between relative flex flex-wrap mt-24 py-20">
         <div
-          class="lg:w-4/12 prose font-primary md:w-full sm:w-full sm:mb-5 md:mb-5 z-10"
+          class="lg:w-5/12 prose font-primary md:w-full sm:w-full sm:mb-5 md:mb-5 z-10"
         >
-          <div class="mx-auto w-9/12 md:w-full justify-center sm:w-full">
+          <div class="mx-auto w-9/12 pr-14 md:w-full justify-center sm:w-full">
             <div class="w-full flex items-center">
               <img src="@/assets/images/home/real-time/mail.png" />
             </div>
             <h1 class="text-4xl mb-5 text-white">Real Time Email Verification Api</h1>
-            <p class="text-xl mb-10 leading-9 text-white">
+            <p class="text-xl mb-10 pr-1 leading-9 text-white">
               As soon as the email is typed in the signup form, our API checks it to
               verify the origin of the email address and filters out the unnecessary ones.
             </p>
           </div>
         </div>
-        <div class="lg:w-6/12 flex relative justify-center md:w-full sm:w-full">
+        <div class="lg:w-7/12 flex pl-1 justify-end md:w-full sm:w-full">
           <img src="@/assets/images/home/bulk-email/table.png" class="" />
         </div>
       </div>
     </div>
   </section>
 </template>
+<script setup>
+
+import { containerMaxW } from "@/config.js";
+</script>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px'
+    },
     asideScrollbars: {
       light: "light",
       gray: "gray",
@@ -13,6 +20,7 @@ module.exports = {
     extend: {
       zIndex: {
         "-1": "-1",
+        "-2": "-2",
       },
       flexGrow: {
         5: "5",
